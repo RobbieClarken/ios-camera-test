d386b098164df4845bd9018e6595317de088471f
RobbieClarken <robbie.clarken@gmail.com>
2020-03-26 17:27:12 +1030

    Initial Commit

---
+++ Snap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snap
+//
+//  Created by Robbie Clarken on 26/3/20.
+//  Copyright © 2020 Robbie Clarken. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
